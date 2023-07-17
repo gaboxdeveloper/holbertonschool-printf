83f785000a14d11fee46d07892bc9721248f0be6
Gabriel <grossy.sjb@gmail.com>
2023-07-17 06:25:25 -0700

    making format functions

---
+++ format_aux.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int (*format_aux(const char *format))(va_list)
+{
+	int i = 0;
+	print_t p[] = {
+		{"c", print_c};
+		{"s", print_s};
+		{"d", print_d};
+		{"i", print_i};
+		{NULL, NULL};
+	}
+
+	for (; p[i].t == NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+			break;
+	}
+	return (p[i].t)
+}
