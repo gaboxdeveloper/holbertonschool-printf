82e5b1aae369a10c32e1bb8f8bde95358a16a994
mabreu88 <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@github.com>
2023-07-17 06:20:30 -0700

    Funtions prototypes

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_d(va_list d);
+int print_i(va_list i);
+int _putchar(char c);
+#endif
